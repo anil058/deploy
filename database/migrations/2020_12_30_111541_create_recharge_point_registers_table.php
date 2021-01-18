8b66902ac638cf94ef3caae6a8f36b95433d7735
anil058 <anil058@gmail.com>
2021-01-18 15:39:33 +0530

    ref_member_id

--- database/migrations/2020_12_30_111541_create_recharge_point_registers_table.php
+++ database/migrations/2020_12_30_111541_create_recharge_point_registers_table.php
@@ -16,6 +16,7 @@ class CreateRechargePointRegistersTable extends Migration
         Schema::create('recharge_point_registers', function (Blueprint $table) {
             $table->id();
             $table->foreignId('member_id')->references('member_id')->on('members')->onDelete('cascade');
+            $table->integer('ref_member_id')->unsigned();
             $table->dateTime('tran_date')->nullable()->index();
             $table->foreignId('payment_id')->nullable()->index();
             $table->foreignId('recharge_id')->nullable()->index();
